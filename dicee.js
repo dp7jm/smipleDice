05c1778ea8176adb9ad5eed4552b6c0f123c781c
D M <odnajwielpiela@gmail.com>
2021-01-20 23:57:30 +0100

    random number generation

--- dicee.js
+++ dicee.js
@@ -1,1 +1,4 @@
-var dices = document.querySelectorAll(".dice");
+var firstRandomNumber = Math.round(Math.random() * 5)+1;
+var secondRandomNumber = Math.round(Math.random() * 5)+1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
