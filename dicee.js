c99984d878e2cb7a335ffffe93733ef9dca04ded
D M <odnajwielpiela@gmail.com>
2021-01-21 00:03:16 +0100

    added functionality of random selction by boath palyers

--- dicee.js
+++ dicee.js
@@ -1,4 +1,8 @@
 var firstRandomNumber = Math.round(Math.random() * 5)+1;
 var secondRandomNumber = Math.round(Math.random() * 5)+1;
+var src1String = "./images/dice" + firstRandomNumber + ".png";
+var src2String = "./images/dice" + secondRandomNumber + ".png";
 
-document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
+document.querySelector(".img1").setAttribute("src", src1String);
+document.querySelector(".img2").setAttribute("src", src2String );
+
